7eebee88c834ca33b9e5cede9e890d9703cedb65
Noah <noenavarrete71@outlook.com>
2023-04-08 11:27:39 -0300

    se agrenga los callback de las rutas

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,30 @@
+const {Bajaj} = require("../models/Bajaj")
 class Controller {
 
-    ver (req, res) {
-        res.send("hola")
+    async ver (req, res) {
+        const bajajs = await Bajaj.find();
+        res.status(200).json(bajajs)
+    }
+
+    async crear (req, res) {
+        try {
+            const bajajGuardar = new Bajaj(req.body)
+            await bajajGuardar.save()
+            res.status(201).json(bajajGuardar)
+        } catch (error) {
+            res.status(501).json({msg: "No se pudo guardar la moto"})
+            
+        }
+    }
+
+    async editar (req, res) {
+        await Bajaj.findByIdAndUpdate(req.params.id, req.body)
+        res.status(202).json({msg:"producto actualizado"})
+    }
+
+    async eliminar (req, res) {
+        await Bajaj.findByIdAndDelete(req.params.id)
+        res.json({msg:"La moto " + req.params.id + " se elimino"})
     }
 }
 
