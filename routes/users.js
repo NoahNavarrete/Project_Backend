fc8e7f3507077114af4826dc6156456c521940ae
Noah <noenavarrete71@outlook.com>
2023-04-08 12:22:34 -0300

    se agregan los middlewares a las rutas

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/userControllers")
+const {validar} = require("../middlewares/validarId")
+const check = require("../middlewares/checks")
+const {validarChecks} = require("../middlewares/validarChecks")
 
 
 router.get("/ver", controller.ver)
-router.post("/crear", controller.crear)
-router.put("/editar/:id", controller.editar)
-router.delete("/eliminar/:id", controller.eliminar)
+router.post("/crear",check, validarChecks, controller.crear)
+router.put("/editar/:id",validar, check, validarChecks, controller.editar)
+router.delete("/eliminar/:id", validar, controller.eliminar)
 
 module.exports = router
