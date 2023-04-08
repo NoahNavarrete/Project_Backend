a3a9cec411f193b9eadb6e233cdcc2c707839f2e
Noah <noenavarrete71@outlook.com>
2023-04-08 11:28:10 -0300

    se agrega connect

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const logger = require("morgan")
 const app = express()
 
 const userRouter = require("./routes/users")
+const {connect} = require("./db/db")
 
 
 app.use(logger("dev"))
 app.use(express.json())
 app.use(cors())
 
+
 app.use("/", userRouter )
+
+connect();
 
 module.exports = app
