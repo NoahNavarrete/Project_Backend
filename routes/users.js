b44182868e37ff0885e91ab33b2b7154641649c0
Noah <noenavarrete71@outlook.com>
2023-04-08 11:26:09 -0300

    se crea el crud basico

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const controller = require("../controllers/userControllers")
 
 
-router.get("/ver",controller.ver)
-
+router.get("/ver", controller.ver)
+router.post("/crear", controller.crear)
+router.put("/editar/:id", controller.editar)
+router.delete("/eliminar/:id", controller.eliminar)
 
 module.exports = router
